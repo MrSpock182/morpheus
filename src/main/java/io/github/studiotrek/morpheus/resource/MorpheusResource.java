0c3634eed8b49cac852fd6d0ff02155970b62c22
MrSpock182 <klebernunes182@gmail.com>
2025-03-30 02:36:37 -0300

    projeto de integracao MKS

---
+++ src/main/java/io/github/studiotrek/morpheus/resource/MorpheusResource.java
@@ -0,0 +1,20 @@
+package io.github.studiotrek.morpheus.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/morpheus/v1")
+public class MorpheusResource {
+
+    @ResponseStatus(OK)
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "SUCCESS";
+    }
+
+}
---
+++ src/test/java/io/github/studiotrek/morpheus/MorpheusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.studiotrek.morpheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
