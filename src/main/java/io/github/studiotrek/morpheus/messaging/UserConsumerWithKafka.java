929863e5558705226cdf38ea3cd1842316917ede
MrSpock182 <klebernunes182@gmail.com>
2025-03-31 15:21:39 -0300

    alterando a deserializacao

--- src/main/java/io/github/studiotrek/morpheus/messaging/UserConsumerWithKafka.java
+++ src/main/java/io/github/studiotrek/morpheus/messaging/UserConsumerWithKafka.java
@@ -4,21 +4,24 @@ import io.github.studiotrek.morpheus.messaging.dto.UserListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserConsumerWithKafka {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserConsumerWithKafka.class);
 
-    //    @RetryableTopic(
-//            autoCreateTopics = "false",
-//            backoff = @Backoff(
-//                    delay = 15000,
-//                    multiplier = 2.0,
-//                    maxDelay = 54000),
-//            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE
-//    )
+    @RetryableTopic(
+            autoCreateTopics = "false",
+            backoff = @Backoff(
+                    delay = 15000,
+                    multiplier = 2.0,
+                    maxDelay = 54000),
+            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE
+    )
     @KafkaListener(
             topics = "${spring.kafka.topic.msk-topic}",
             groupId = "${spring.kafka.consumer.group-id}",
